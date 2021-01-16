bdb93d25dc1bc0002c6f092ffb28ae8d184f4d0b
enfow <wgm0601@gmail.com>
2021-01-17 01:11:59 +0900

    Add get_market_and_currency to global_stock.py

--- crawler/global_stock.py
+++ crawler/global_stock.py
@@ -8,25 +8,52 @@ Notes:
     changed, it may not work.
 """
 
+from typing import Tuple
+
 import requests
 from bs4 import BeautifulSoup
 
 
-def get_stock_info(code: str) -> str:
-    """Get stock information from naver."""
+def get_stock_info(code: str) -> Tuple[float, str, str]:
+    """Get stock information from naver.
+
+    Returns:
+        - current price(str)
+        - market(str) - NYSE, KOSPI etc
+        - currency(str) - USD, KRW
+    """
     url = f"https://finance.yahoo.com/quote/{code}?p={code}&.tsrc=fin-srch"
     html_txt = BeautifulSoup(requests.get(url).content, "html.parser")
     cur_price = get_current_price(html_txt)
+    market, currency = get_market_and_currency(html_txt)
+
+    return cur_price, market, currency
+
+
+def get_current_price(html_txt: BeautifulSoup) -> float:
+    """Get current price.
+
+    Returns:
+        - current price(str)
+    """
+    shell = html_txt.find("div", {"id": "mrt-node-Lead-3-QuoteHeader"})
+    shell = shell.find("span", {"data-reactid": "32"})
+    cur_price = float(shell.text.replace(",", ""))
     return cur_price
 
 
-def get_current_price(html_txt: BeautifulSoup) -> str:
-    """Get current price."""
+def get_market_and_currency(html_txt: BeautifulSoup) -> Tuple[str, str]:
+    """Get current price.
+
+    Returns:
+        - market name(str)
+        - currency price(str)
+    """
     shell = html_txt.find("div", {"id": "mrt-node-Lead-3-QuoteHeader"})
-    shell = shell.find("span", {
-        "class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"
-    })
-    return shell.text
+    shell = shell.find("span", {"data-reactid": "9"})
+    shell_words = shell.text.split(" ")
+    market, currency = shell_words[0], shell_words[-1]
+    return market, currency
 
 
 if __name__ == "__main__":
