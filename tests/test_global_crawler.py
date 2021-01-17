445e099d1a653a3473a3b8e7a2c756ad99406d15
enfow <wgm0601@gmail.com>
2021-01-17 10:42:30 +0900

    Create test for exchange_rate getter

--- tests/test_global_crawler.py
+++ tests/test_global_crawler.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 from crawler.global_stock import (get_current_price, get_market_and_currency,
                                   get_stock_info)
+from crawler.exchange_rate import get_exchange_rate
 
 VALID_TICKERS = ["005930.KS", "T", "035760.KQ", "INTC"]
 VALID_MARKETS = ["KSE", "NYSE", "KOSDAQ", "NasdaqGS"]
@@ -26,10 +27,10 @@ class TestGlobalCrwaler:
             )
 
     def test_get_current_price(self):
-        """Check the module get the current price of the stock.
+        """Check the function get_current_price() get the current price of the stock.
 
         Notes:
-            - The current price of the stock change almost everytime. So It
+            - The current price of the stock changes almost everytime. So It
             just check the data type.
         """
         for html_txt in self.html_txts:
@@ -38,8 +39,24 @@ class TestGlobalCrwaler:
             assert cur_price > 0.0
 
     def test_get_market_and_currency(self):
-        """Check the module get the exact market name and currency."""
+        """Check the the function get_market_and_currency() get the exact market name 
+        and currency.
+        """
         for idx, html_txt in enumerate(self.html_txts):
             market, currency = get_market_and_currency(html_txt)
             assert market == VALID_MARKETS[idx]
             assert currency == VALID_CURRENCY[idx]
+
+    def test_get_exchange_rate(self):
+        """Check the function get_exchange_rate() get the current price of the stock.
+
+        Notes:
+            - The current exchange rate changes almost everytime. So It just check 
+            the data type.
+        """
+        used_currencies = set(VALID_CURRENCY)
+        used_currencies.remove("USD")
+        for currency in used_currencies:
+            cur_exchange = get_exchange_rate(currency)
+            assert isinstance(cur_exchange, float)
+            assert cur_exchange > 0.0
